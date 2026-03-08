8654ff52b5520070bd704c305e385da248023a21
Bridget <bridget.tang@yahoo.com>
2026-03-08 15:44:13 +1000

    Applying style in give kudos page

--- src/components/PreviewCard.jsx
+++ src/components/PreviewCard.jsx
@@ -19,27 +19,29 @@ function PreviewCard() {
             </div>
 
             <div className="preview-content">
+
+                <div className="preview-misc">
+                    <span>Just Now</span> <span>{visibility}</span>
+                </div>
+
                 <div className="preview-sender-recipients">
                     <span
                         key={currentSender.id}
-                        className="recipient"
-                    >
+                        className="recipient">
                         <div className="avatar">{currentSender.initial}</div>
                         <span>{currentSender.name}</span>
                     </span>
                     ↓
                     <div className="recipients-grid">
-                        {selectedRecipients.map((recipient) => (<span
-                            key={recipient.id}
-                            className="recipient"
-                        >
-                        <div className="avatar">{recipient.initial}</div>
-                        <span>{recipient.name}</span>
-                    </span>))}
+                        {selectedRecipients.map((recipient) => (
+                            <span
+                                key={recipient.id}
+                                className="recipient">
+                                <div className="avatar">{recipient.initial}</div>
+                                <span>{recipient.name}</span>
+                            </span>))}
                     </div>
                 </div>
-
-                <div className="preview-label">Just now</div>
 
                 <div className="preview-skill">
                     <div className="skills-grid">
@@ -59,18 +61,14 @@ function PreviewCard() {
 
                 {mediaImage ? (
                     <div className="preview-image">
-                        <div className="image-preview" aria-hidden="true"><img src={mediaImage} alt="Kudos image"/>
+                        <div className="image-preview fix-width" aria-hidden="true"><img src={mediaImage} alt="Kudos image"/>
                         </div>
                     </div>) : null}
 
                 {mediaLink ? (<div className="preview-link">
-                    <div className="link-preview" aria-hidden="true"><img src={mediaLink} alt="Kudos link"/>
+                    <div className="link-preview fix-width" aria-hidden="true"><img src={mediaLink} alt="Kudos link"/>
                     </div>
                 </div>) : null}
-
-                <div className="preview-visibility">
-                    {visibility}
-                </div>
             </div>
         </div>
     );
