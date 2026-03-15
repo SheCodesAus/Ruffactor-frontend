2de9f4e31ef35ff6370133edecd34800e0cf48bb
Bridget <bridget.tang@yahoo.com>
2026-03-15 18:53:03 +1000

    Adding updating kudos page

--- src/pages/GiveKudos.jsx
+++ src/pages/GiveKudos.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import GiveKudosMain from "../components/GiveKudosMain.jsx";
-import GiveKudosSide from "../components/GiveKudosSide.jsx";
 import {EditingKudosProvider} from "../context/EditingKudosContext.jsx";
 import "../components/GiveKudos.css";
+import EditingKudosForm from "../components/EditingKudosForm.jsx";
 
 function GiveKudos() {
     return (
         <EditingKudosProvider>
-            <div className="give-kudos-container">
-                <GiveKudosMain/>
-                <GiveKudosSide/>
-            </div>
+            <EditingKudosForm/>
         </EditingKudosProvider>
     );
 }
